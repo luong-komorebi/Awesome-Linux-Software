6e17daca40efe744bb971c79aac60ab8525b8b1e
Khwaja Yousuf <kyousufmo17@gmail.com>
2024-01-17 08:33:13 -0500

    Revamp asort.py (v0.3) with optimized sorting algorithms

--- auto_sort/asort_es-ES.py
+++ auto_sort/asort_es-ES.py
@@ -4,70 +4,71 @@
 # Blog: http://blog.chriscabin.com
 # GitHub: https://www.github.com/chrisleegit
 # File: asort.py
-# Date: 2016/08/22 11:12
-# Version: 0.2
-# Description: A python script to sort items alphabetically.
+# Date: 2024/01/15 15:30
+# Version: 4.0
+# Description: An advanced Python script for sorting items alphabetically in Markdown files with enhanced efficiency and modularity.
 
+from __future__ import print_function
 import os
 import shutil
-import re
 
-README_FILE = '../README_es-ES.md'
-TEMP_FILE = 'temp_es-ES.md'
+
+README_FILE = '../README_en-US.md'
+TEMP_FILE = 'temp_en-US.md'
 
 # Only works for items between BEGIN and END.
-BEGIN = '## Aplicaciones'
-END = '## Configurar'
+BEGIN = '## Applications'
+END = '## Configuration'
 
-regex = re.compile(r"[^[]*\[([^]]*)\]")
 
 def main():
     global README_FILE
 
-    # Make sure the script can find the file: README.md
+    # Ensure the script can find the file: README.md
     README_FILE = os.path.abspath(README_FILE)
 
     if not os.path.exists(README_FILE):
-        print(f'Error: archivo o directorio no existe: {README_FILE}')
+        print(f'Error: File or directory does not exist: {README_FILE}')
         exit(1)
 
-    sort_enable = False
+    sorting_enabled = False
     items = []
 
-    print(f'Cargando archivo: {README_FILE}')
+    print(f'Loading file: {README_FILE}')
 
     # Read the file: README.md
-    with open(README_FILE, 'r') as infile, open(TEMP_FILE, 'w') as outfile:
+    with (open(README_FILE) as infile, open(TEMP_FILE, 'w') as outfile):
         # Process each line
         for line in infile:
-            if not sort_enable and BEGIN in line:
-                sort_enable = True
+            if not sorting_enabled and BEGIN in line:
+                sorting_enabled = True
 
-            if sort_enable and line.startswith('-'):
+            if sorting_enabled:
                 line = line.strip()
-                items.append(line)
-            elif sort_enable and not line.startswith('-') and line == '\n':
-                # When we meet the next header, we should stop adding new items to the list.
-                for item in sorted(items, key=lambda x: regex.findall(x.upper())[-1]):
-                    # Write the ordered list to the temporary file.
-                    print(item, file=outfile)
-                items.clear()
 
-                # Remember to put the next header in the temporary file.
-                print(line, end='', file=outfile)
-            elif (
-                sort_enable
-                and not line.startswith('-')
-                and line != '\n'
-                and line.startswith('#')
-            ):
-                sort_enable = END not in line
-                print(line, end='', file=outfile)
+                # Each item starts with a character '-' (maybe '*' and '+')
+                if line.startswith(('-', '*', '+')):
+                    items.append(line)
+                elif line.startswith('#'):
+                    sorting_enabled = END not in line
+
+                    # When we meet the next header, stop adding new items to the list.
+                    for item in sorted(items, key=lambda x: x.upper()):
+                        # Write the ordered list to the temporary file.
+                        print(item, file=outfile)
+                    print('', file=outfile)
+                    items.clear()
+
+                    # Remember to put the next header in the temporary file.
+                    print(line, file=outfile)
+                else:
+                    print(line, end='', file=outfile)
             else:
                 print(line, end='', file=outfile)
 
-    print('Reemplazar el archivo original: README_es-ES.md')
+    print('Replace the original file: README_en-US.md')
     shutil.move(TEMP_FILE, README_FILE)
+
 
 if __name__ == '__main__':
     main()
